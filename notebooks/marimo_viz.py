921eaa0f1e5af3b98f8155ef80dd87263364de87
Fernando Masumoto <fernando.masumoto@gmail.com>
2026-05-29 22:16:03 -0300

    exploring marimo ui elements

--- notebooks/marimo_viz.py
+++ notebooks/marimo_viz.py
@@ -1,10 +1,10 @@
 import marimo
 
 __generated_with = "0.19.11"
-app = marimo.App(width="medium")
+app = marimo.App(width="columns")
 
 
-@app.cell(hide_code=True)
+@app.cell(column=0, hide_code=True)
 def _(mo):
     mo.md(r"""
     # Marimo interactive visualization
@@ -17,15 +17,22 @@ def _():
     import marimo as mo
     import numpy as np
     import altair as alt
-    from altair.datasets import data
+
     import polars as pl
     import matplotlib.pyplot as plt
 
-
-    # Statistics
-    from statsmodels.tsa.seasonal import seasonal_decompose
+    # Altair datasets 
+    from altair.datasets import data
 
     return alt, data, mo, pl
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### Enabling vegafusion for bigger datasets
+    """)
+    return
 
 
 @app.cell
@@ -38,14 +45,44 @@ def _(alt):
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
-    # Importing Weather dataset
+    # Choosing Dataset
     """)
     return
 
 
 @app.cell
+def _(mo):
+    dropdown = mo.ui.dropdown(options=["Apples", "Oranges", "Pears"], label="choose fruit")
+    dropdown_dict = mo.ui.dropdown(options={"Apples":1, "Oranges":2, "Pears":3},
+                            value="Apples", # initial value
+                            label="choose fruit with dict options")
+    return dropdown, dropdown_dict
+
+
+@app.cell(hide_code=True)
+def _(dropdown, dropdown_dict, mo):
+    mo.vstack([mo.hstack([dropdown, mo.md(f"Has value: {dropdown.value}")]),
+    mo.hstack([dropdown_dict, mo.md(f"Has value: {dropdown_dict.value} and selected_key {dropdown_dict.selected_key}")]),
+                ])
+    return
+
+
+@app.cell
+def _(data, mo):
+    datasets_dropdown = mo.ui.dropdown(options=data.list_datasets(), label="Choose the dataset")
+    datasets_dropdown
+    return
+
+
+@app.cell
 def _(data, pl):
-    df = pl.from_dataframe(data.weather())
+    pl.from_dataframe(getattr(data, 'weather')())
+    return
+
+
+@app.cell
+def _(data, pl):
+    df = pl.from_dataframe(data.weather()) 
     return (df,)
 
 
@@ -66,14 +103,16 @@ def _(mo):
 @app.cell
 def _(df, pl):
     df2 = df.with_columns(
-        pl.col('date').dt.year().alias('year'),
-        pl.col('date').dt.month().alias('month'),
+        pl.col("date").dt.year().alias("year"),
+        pl.col("date").dt.month().alias("month"),
         pl.concat_str(
-            pl.col('date').dt.year(),
-            pl.col('date').dt.strftime('%V'),
-        ).str.to_integer().alias('weekyear'),
-        pl.col('date').dt.weekday().alias('weekday'),
-        pl.col('date').dt.week().alias('isoweek'),
+            pl.col("date").dt.year(),
+            pl.col("date").dt.strftime("%V"),
+        )
+        .str.to_integer()
+        .alias("weekyear"),
+        pl.col("date").dt.weekday().alias("weekday"),
+        pl.col("date").dt.week().alias("isoweek"),
     )
     return (df2,)
 
@@ -103,88 +142,106 @@ def _(mo):
 
 
 @app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell(column=1)
+def _():
+    return
+
+
+@app.cell(hide_code=True)
 def _(alt, df2, mo):
-    chart1 = alt.Chart(df2).mark_line().encode(
-        x=alt.X('date', title='Date'),
-        y=alt.Y('wind:Q', title='Wind Velocity (mm)'),
-        color='location:N',
-        row=alt.Row('location', header=None)
-    ).properties(
-        height=200,
-        width=800
+    chart1 = (
+        alt.Chart(df2)
+        .mark_line()
+        .encode(
+            x=alt.X("date", title="Date"),
+            y=alt.Y("wind:Q", title="Wind Velocity (mm)"),
+            color="location:N",
+            row=alt.Row("location", header=None),
+        )
+        .properties(height=200, width=800)
     )
 
 
-    chart2 = alt.Chart(df2).mark_tick().encode(
-        # x=alt.X('precipitation', title='Count'),
-        y=alt.Y('wind',
+    chart2 = (
+        alt.Chart(df2)
+        .mark_tick()
+        .encode(
+            # x=alt.X('precipitation', title='Count'),
+            y=alt.Y(
+                "wind",
                 scale=alt.Scale(domainMin=0),
-                axis=alt.Axis(orient='right', labels=False, ticks=False, title=None)),
-        color=alt.Color('location:N', legend=alt.Legend(orient="top", title='Location')),
-        row=alt.Row('location', header=None)
-    ).properties(
-        height=200,
-        width=20,
+                axis=alt.Axis(
+                    orient="right", labels=False, ticks=False, title=None
+                ),
+            ),
+            color=alt.Color(
+                "location:N", legend=alt.Legend(orient="top", title="Location")
+            ),
+            row=alt.Row("location", header=None),
+        )
+        .properties(
+            height=200,
+            width=20,
+        )
     )
 
     grouped_chart = chart1 | chart2
 
     mo.ui.altair_chart(grouped_chart)
-    return
-
-
-@app.cell
-def _():
-    return
-
-
-@app.cell
-def _():
-    return
-
-
-@app.cell
-def _():
-    return
-
-
-@app.cell
-def _():
-    return
-
-
-@app.cell
-def _():
-    return
-
-
-@app.cell
-def _():
-    return
-
-
-@app.cell
-def _():
-    return
-
-
-@app.cell
-def _():
-    return
-
-
-@app.cell
-def _():
-    return
-
-
-@app.cell
-def _():
-    return
-
-
-@app.cell
-def _():
     return
 
 
